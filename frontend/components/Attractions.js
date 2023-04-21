fa9d4562c3c1053267341b1a63a2ce8b59d5ba9f
Glenn de Haan <glenn@dehaan.cloud>
2023-04-21 15:46:36 +0200

    Updated history text

--- frontend/components/Attractions.js
+++ frontend/components/Attractions.js
@@ -210,8 +210,8 @@ export default class Attractions extends Component {
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
                                                 </svg>
                                                 {!showWaitTimeMetric && item.history.waitTime.standby.minutes === item.waitTime.standby.minutes && <span className="inline-block align-middle">Average Wait</span>}
-                                                {!showWaitTimeMetric && item.history.waitTime.standby.minutes < item.waitTime.standby.minutes && <span className="inline-block align-middle">Long Wait</span>}
-                                                {!showWaitTimeMetric && item.history.waitTime.standby.minutes > item.waitTime.standby.minutes && <span className="inline-block align-middle">Short Wait</span>}
+                                                {!showWaitTimeMetric && item.history.waitTime.standby.minutes < item.waitTime.standby.minutes && <span className="inline-block align-middle">Longer Wait</span>}
+                                                {!showWaitTimeMetric && item.history.waitTime.standby.minutes > item.waitTime.standby.minutes && <span className="inline-block align-middle">Shorter Wait</span>}
                                                 {showWaitTimeMetric && <span className="inline-block align-middle">{item.history.waitTime.standby.minutes} min</span>}
                                             </span>
                                         }
